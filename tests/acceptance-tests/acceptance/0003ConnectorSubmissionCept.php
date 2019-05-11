2efe9de6f63bf954713c286b3a3a0c0eafe31352
Steve Henty <stevehenty@gmail.com>
2019-05-11 18:46:39 +0200

    Refactor submit_page setting adjustment.

--- tests/acceptance-tests/acceptance/0003ConnectorSubmissionCept.php
+++ tests/acceptance-tests/acceptance/0003ConnectorSubmissionCept.php
@@ -8,20 +8,6 @@ $I = new AcceptanceTester( $scenario );
 
 $I->wantTo( 'Test the Form Connector with a submission step' );
 
-// Update the target page in the submission step
-$form_id = GFFormsModel::get_form_id( '0003 FormConnector - Source' );
-$steps = gravity_flow()->get_steps( $form_id );
-foreach ( $steps as $step ) {
-	if ( $step->get_name() == 'Form Submission Source' ) {
-
-		$submit_page = get_page_by_title( '0003 FormConnector - Destination' );
-		$feed_meta = $step->get_feed_meta();
-		$feed_meta['submit_page'] = $submit_page->ID;
-		gravity_flow()->update_feed_meta( $step->get_id(), $feed_meta );
-	}
-}
-
-
 // Submit the form
 $I->amOnPage( '/0003-formconnector-source' );
 
